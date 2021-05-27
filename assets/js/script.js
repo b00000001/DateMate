f39b4f3408f8e3dcf6e81eeb5c5c25fc5757e5fe
Aaron Deas <deas.aaron@gmail.com>
2021-05-27 16:26:00 -0400

    Changed logic for script slightly, cleaned up existing functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 //  ---------------------------------------------
 var spoonAPIKey = "e29435235c7a48c3b173e38c7d69df99";
 var recipeDisplayDiv = document.getElementById("recipe__data");
+var movieDataDiv = document.getElementById("movie__data");
 var randomfoodUrl =
 	"https://api.spoonacular.com/recipes/random?apiKey=" + spoonAPIKey;
-var testVar;
+
+var testVar; // Reserved variable for holding any API data for testing purposes.
 var movieGenres = [
 	28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770,
 	53, 10752, 37,
 ]; // These are genre codes for the API to use to select random movies from this array of genres ids.
 var movieDbKey = "8e39c89d5fa028e82010a11d982e8911";
+var genresUrl =
+	"https://api.themoviedb.org/3/discover/movie?api_key=" +
+	movieDbKey +
+	"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_genres=" +
+	movieGenres[Math.floor(Math.random() * movieGenres.length)] +
+	"&with_watch_monetization_types=flatrate";
 var apiTestButton = document.getElementById("testapi");
 var randomMovieButton = document.getElementById("random__movie");
 randomMovieButton.addEventListener("click", callMovieDb);
@@ -16,29 +24,16 @@ apiTestButton.addEventListener("click", function () {
 	callSpoonacularApi(randomfoodUrl);
 });
 
-function callMovieDb(url) {
-	var url =
-		"https://api.themoviedb.org/3/discover/movie?api_key=" +
-		movieDbKey +
-		"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_genres=" +
-		movieGenres[Math.floor(Math.random() * movieGenres.length)] +
-		"&with_watch_monetization_types=flatrate";
-	fetch(url)
+function callMovieDb() {
+	movieDataDiv.innerText = "";
+
+	fetch(genresUrl)
 		.then(function (res) {
 			return res.json();
 		})
 		.then(function (movies) {
 			console.log(movies);
-			var movieDataDiv = document.getElementById("movie__data");
-			var h2El = document.createElement("h2");
-			var pEl = document.createElement("p");
-			var movieRandomPick =
-				movies.results[Math.floor(Math.random() * movies.results.length)];
-
-			h2El.innerText = movieRandomPick.original_title;
-			pEl.innerText = movieRandomPick.overview;
-			movieDataDiv.appendChild(h2El);
-			movieDataDiv.appendChild(pEl);
+			displayMovie(movies);
 		})
 		.catch(function (error) {
 			console.log(error);
@@ -53,38 +48,54 @@ function callSpoonacularApi(url) {
 		})
 		.then(function (foods) {
 			console.log(foods);
-			var ptag = document.createElement("p");
-			var h3El = document.createElement("h3");
-			h3El.innerText = foods.recipes[0].title;
+			displayRecipe(foods);
+		});
+}
+function displayMovie(movies) {
+	var h2El = document.createElement("h2");
+	var pEl = document.createElement("p");
+	var movieRandomPick =
+		movies.results[Math.floor(Math.random() * movies.results.length)];
 
-			var ingredientType = foods.recipes[0].extendedIngredients[0].nameClean;
-			console.log(ingredientType);
-			ptag.innerHTML = foods.recipes[0].instructions;
-			recipeDisplayDiv.appendChild(h3El);
-			recipeDisplayDiv.appendChild(ptag);
-			var winePairingUrl =
-				"https://api.spoonacular.com/food/wine/pairing?food=" +
-				ingredientType +
-				"&apiKey=" +
-				spoonAPIKey;
-			fetch(winePairingUrl)
-				.then(function (res) {
-					return res.json();
-				})
-				.then(function (winePairing) {
-					console.log(winePairing);
-					var wineDiv = document.createElement("div");
-					var h4El = document.createElement("h4");
-					if (winePairing.pairingText === "") {
-						h4El.innerHTML = "No Suggested wine for this dinner, Enjoy!";
-					} else {
-						h4El.innerHTML = winePairing.pairingText;
-					}
-					wineDiv.appendChild(h4El);
-					recipeDisplayDiv.appendChild(wineDiv);
-				});
+	h2El.innerText = movieRandomPick.original_title;
+	pEl.innerText = movieRandomPick.overview;
+	movieDataDiv.appendChild(h2El);
+	movieDataDiv.appendChild(pEl);
+}
+function displayRecipe(foods) {
+	var ptag = document.createElement("p");
+	var h3El = document.createElement("h3");
+	h3El.innerText = foods.recipes[0].title;
+	var ingredientType = foods.recipes[0].extendedIngredients[0].nameClean;
+	console.log(ingredientType);
+	ptag.innerHTML = foods.recipes[0].instructions;
+	recipeDisplayDiv.appendChild(h3El);
+	recipeDisplayDiv.appendChild(ptag);
+	var winePairingUrl =
+		"https://api.spoonacular.com/food/wine/pairing?food=" +
+		ingredientType +
+		"&apiKey=" +
+		spoonAPIKey;
+	fetch(winePairingUrl)
+		.then(function (res) {
+			return res.json();
+		})
+		.then(function (winePairing) {
+			displayWine(winePairing);
 		})
 		.catch(function (error) {
 			console.log(error);
 		});
+}
+function displayWine(winePairing) {
+	console.log(winePairing);
+	var wineDiv = document.createElement("div");
+	var h4El = document.createElement("h4");
+	if (winePairing.pairingText === "") {
+		h4El.innerHTML = "No Suggested wine for this dinner, Enjoy!";
+	} else {
+		h4El.innerHTML = winePairing.pairingText;
+	}
+	wineDiv.appendChild(h4El);
+	recipeDisplayDiv.appendChild(wineDiv);
 }

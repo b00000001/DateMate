4bc848156c5aa3250a7ee1aa459c8caa34f615a0
Aaron Deas <deas.aaron@gmail.com>
2021-05-27 15:34:56 -0400

    worked out a few bugs in API calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var testVar;
 var movieGenres = [
 	28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770,
 	53, 10752, 37,
-];
+]; // These are genre codes for the API to use to select random movies from this array of genres ids.
 var movieDbKey = "8e39c89d5fa028e82010a11d982e8911";
 var apiTestButton = document.getElementById("testapi");
 var randomMovieButton = document.getElementById("random__movie");

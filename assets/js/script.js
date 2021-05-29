798b3e12b6367fb0377f01675295a5e340cf5364
Aaron Deas <deas.aaron@gmail.com>
2021-05-29 03:39:06 -0400

    Added a test layout and connected buttons. Perhaps this design may be more functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //  ----------------------API Related variables ----------------------
 var spoonAPIKey = "e29435235c7a48c3b173e38c7d69df99";
 var recipeDisplayDiv = document.getElementById("recipe__column");
+recipeDisplayDiv.setAttribute("class", "ml-4 mr-4");
 var movieTitleBtn = document.getElementById("movie__title");
 var randomfoodUrl =
 	"https://api.spoonacular.com/recipes/random?apiKey=" + spoonAPIKey;
@@ -11,6 +12,7 @@ var movieGenres = [
 	28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770,
 	53, 10752, 37,
 ]; // These are genre codes for the API to use to select random movies from this array of genres ids.
+var movieGenre = document.getElementById("movie__genres");
 var movieDbKey = "8e39c89d5fa028e82010a11d982e8911";
 var genresUrl =
 	"https://api.themoviedb.org/3/discover/movie?api_key=" +
@@ -28,7 +30,8 @@ randomRecipeButton.addEventListener("click", () => {
 });
 //  ----------------API Call Functions ------------------------------
 var callMovieDb = () => {
-	moviePlot.children[0].innerText = "";
+	moviePlot.innerText = "";
+	movieGenre.innerText = "";
 	console.log("test");
 
 	fetch(genresUrl)
@@ -57,13 +60,18 @@ var displayMovie = (movies) => {
 	var h2El = document.createElement("h2");
 	var pEl = document.createElement("p");
 	var movieTitle = document.getElementById("movie__title");
+	var moviePoster = document.getElementById("poster__icon");
 	var movieRandomPick =
 		movies.results[Math.floor(Math.random() * movies.results.length)];
 	getGenre(movieRandomPick);
 	movieTitle.innerText = movieRandomPick.original_title;
+	moviePoster.setAttribute(
+		"src",
+		"https://image.tmdb.org/t/p/w500/" + movieRandomPick.poster_path
+	);
 	// Movie Title
 	pEl.innerText = movieRandomPick.overview; // Movie Summary
-	moviePlot.children[0].appendChild(pEl);
+	moviePlot.appendChild(pEl);
 };
 var getGenre = (randomMov) => {
 	fetch(
@@ -78,9 +86,9 @@ var getGenre = (randomMov) => {
 						Math.floor(Math.random() * Object.keys(randomMov.genre_ids).length)
 					]
 				) {
-					var h4El = document.createElement("h4");
-
-					// h4El.innerText = "Genre: " + genreList.genres[i].name;
+					var pEl = document.createElement("p");
+					pEl.innerText = "Genre: " + genreList.genres[i].name;
+					movieGenre.appendChild(pEl);
 					console.log("Genre: ", genreList.genres[i].name); // This is how to get the Genre name
 				}
 			}
@@ -92,12 +100,15 @@ var getGenre = (randomMov) => {
 
 var displayRecipe = (foods) => {
 	var ptag = document.createElement("p");
+	ptag.setAttribute("class", "mt-3");
 	var h3El = document.createElement("h3");
-	h3El.innerText = foods.recipes[0].title;
+	var strongTag = document.createElement("strong");
+	strongTag.innerText = foods.recipes[0].title;
+
 	var ingredientType = foods.recipes[0].extendedIngredients[0].nameClean;
 	console.log(ingredientType);
 	ptag.innerHTML = foods.recipes[0].instructions;
-	recipeDisplayDiv.appendChild(h3El);
+	recipeDisplayDiv.appendChild(strongTag);
 	recipeDisplayDiv.appendChild(ptag);
 	var winePairingUrl =
 		"https://api.spoonacular.com/food/wine/pairing?food=" +
@@ -116,6 +127,7 @@ var displayRecipe = (foods) => {
 var displayWine = (winePairing) => {
 	console.log(winePairing);
 	var wineDiv = document.createElement("div");
+	wineDiv.setAttribute("class", "mt-3");
 	var h4El = document.createElement("h4");
 	if (winePairing.pairingText === "") {
 		h4El.innerHTML = "No Suggested wine for this recipe, Enjoy!"; // If there is no returned wine suggestion based on the passed value, just put a default message.
